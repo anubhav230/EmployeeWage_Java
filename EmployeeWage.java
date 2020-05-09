9ef77b3ece7bd05ceeadc7d4259739e86f2fbce8
anubhav230 <anubhavsingh230@gmail.com>
2020-05-10 02:49:35 +0530

    Per day salary check

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,11 +1,16 @@
 public class EmployeeWage {
 	public static void main(String[] args) {
    	int isPresent = 1;
-      double empCheck = Math.floor(Math.random() * 10) % 2;
-      System.out.println(empCheck);
-      if (empCheck == isPresent)
-      	System.out.println("present");
-      else
-      	System.out.println("absent");
+	int empRatePrHr = 20;
+	int empHr = 8;
+    double empCheck = Math.floor(Math.random() * 10) % 2;
+    System.out.println(empCheck);
+    if (empCheck == isPresent) {
+		System.out.println("employee is present");
+		int perDaySalary = empRatePrHr * empHr;
+		System.out.println("Employee per day salary = "+perDaySalary);
+	}
+    else
+		System.out.println("absent");
 	}
 }
