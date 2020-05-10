32f41145ebf854f08da57c587b7714f041d70c5d
anubhav230 <anubhavsingh230@gmail.com>
2020-05-10 23:17:45 +0530

    salary calculated for 100 hours or 20 days

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,38 +1,41 @@
 public class EmployeeWage {
 	void empSalary() {
-		int isFullTime = 0;
-		int isPartTime = 2;
+		int isFullTime = 1;
+		int isPartTime = 0;
 		int empRatePrHr = 20;
 		int empHr = 0;
 		int perDaySalary = 0;
 		int mothWorkingDay = 20;
 		int monthlySalary = 0;
-		for (int i = 1;i <= mothWorkingDay;i++)
-		{
+		int monthyHrs = 0;
+		for (int i = 1;i <= mothWorkingDay && monthyHrs <= 100;i++) {
 			double empCheck = Math.floor(Math.random() * 10) % 2;
 			switch ((int)empCheck)
 			{
-				case 0 :
+				case 1 :
 					empHr = 8;
 					break;
-
-				case 1 :
+				case 2 :
 					empHr = 4;
 					break;
 
 			}//switch
+			monthyHrs = monthyHrs + empHr;
+			System.out.println("working days : "+i);
+			System.out.println("monthyHrs wis : "+monthyHrs);
 			perDaySalary = empRatePrHr * empHr;
-
+			System.out.println(perDaySalary);
 			monthlySalary = perDaySalary + monthlySalary;
 
-		}
-		System.out.println("Monthly salary: "+monthlySalary);
 
-	}
+		}//forloop
+		System.out.println("monthly salary"+monthlySalary);
+
+	}//empWage()
 	public static void main(String[] args) {
+
 		EmployeeWage empWage = new EmployeeWage();
 		empWage.empSalary();
 
 	}//main()
 }//class
-
