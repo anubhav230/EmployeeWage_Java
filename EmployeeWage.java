5c3789187e69c473fcd0506dc10e8afec32633ab
anubhav230 <anubhavsingh230@gmail.com>
2020-05-12 12:14:38 +0530

    part time added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,26 +1,36 @@
 public class EmployeeWage {
-	public static void main(String[] args) {
-   	int isFullTime = 0;
+	void empSalary() {
+		int isFullTime = 1;
 		int isPartTime = 2;
 		int empRatePrHr = 20;
 		int empHr = 0;
 		int perDaySalary = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		System.out.println(empCheck);
 		switch ((int)empCheck)
 		{
-			case 0 :
+			case 1 :
 				empHr = 8;
 				System.out.println("Full time Employee");
 				perDaySalary = empRatePrHr * empHr;
 				System.out.println("Employee per day salary = "+perDaySalary);
-				return;
-			case 1 :
+				break;
+			case 2 :
 				empHr = 4;
 				System.out.println("Part time Employee");
 				perDaySalary = empRatePrHr * empHr;
 				System.out.println("Employee per day salary = "+perDaySalary);
+				break;
+			default :
+				empHr = 0;
+				perDaySalary = 0;
+				System.out.println("Employee per day salary = "+perDaySalary);
+		}//switch
+	}
+	public static void main(String[] args) {
 
-		}//switchs
+		EmployeeWage empWage = new EmployeeWage();
+		empWage.empSalary();
+
 	}//main()
 }//class
